e6804dde6959e5d68e1e5ef1288ce6613b2b72f4
Timothy C. Wright <tcw321@gmail.com>
2016-02-13 15:24:53 -0500

    replace in game the simple Players object

--- python/trivia.py
+++ python/trivia.py
@@ -6,7 +6,7 @@ class Players:
         self.players = []
 
     def length(self):
-        return 0;
+        return len(self.players);
 
     def append(self, name_):
         self.players.append(name_)
@@ -59,22 +59,22 @@ class Game:
         return self.players.length()
     
     def roll(self, roll):
-        print("%s is the current player" % self.players[self.current_player])
+        print("%s is the current player" % self.players.get_player_name(self.current_player))
         print("They have rolled a %s" % roll)
 
         if self.in_penalty_box[self.current_player] and roll % 2 == 0:
-            print("%s is not getting out of the penalty box" % self.players[self.current_player])
+            print("%s is not getting out of the penalty box" % self.players.get_player_name(self.current_player))
             self.is_getting_out_of_penalty_box = False
         else:
             if roll % 2 != 0  and self.in_penalty_box[self.current_player]:
-                print("%s is getting out of the penalty box" % self.players[self.current_player])
+                print("%s is getting out of the penalty box" % self.players.get_player_name(self.current_player))
                 self.is_getting_out_of_penalty_box = True
 
             self.places[self.current_player] = self.places[self.current_player] + roll
             if self.places[self.current_player] > 11:
                 self.places[self.current_player] = self.places[self.current_player] - 12
             
-            print(self.players[self.current_player] + \
+            print(self.players.get_player_name(self.current_player)+ \
                         '\'s new location is ' + \
                         str(self.places[self.current_player]))
             print("The category is %s" % self._current_category)
@@ -104,19 +104,19 @@ class Game:
             if self.is_getting_out_of_penalty_box:
                 print('Answer was correct!!!!')
                 self.purses[self.current_player] += 1
-                print(self.players[self.current_player] + \
+                print(self.players.get_player_name(self.current_player) + \
                     ' now has ' + \
                     str(self.purses[self.current_player]) + \
                     ' Gold Coins.')
                 
                 winner = self._did_player_win()
                 self.current_player += 1
-                if self.current_player == len(self.players): self.current_player = 0
+                if self.current_player == self.players.length(): self.current_player = 0
                 
                 return winner
             else:
                 self.current_player += 1
-                if self.current_player == len(self.players): self.current_player = 0
+                if self.current_player == self.players.length(): self.current_player = 0
                 return True
             
             
@@ -125,24 +125,24 @@ class Game:
             
             print("Answer was corrent!!!!")
             self.purses[self.current_player] += 1
-            print(self.players[self.current_player] + \
+            print(self.players.get_player_name(self.current_player) + \
                 ' now has ' + \
                 str(self.purses[self.current_player]) + \
                 ' Gold Coins.')
             
             winner = self._did_player_win()
             self.current_player += 1
-            if self.current_player == len(self.players): self.current_player = 0
+            if self.current_player == self.players.length(): self.current_player = 0
             
             return winner
     
     def wrong_answer(self):
         print('Question was incorrectly answered')
-        print(self.players[self.current_player] + " was sent to the penalty box")
+        print(self.players.get_player_name(self.current_player) + " was sent to the penalty box")
         self.in_penalty_box[self.current_player] = True
         
         self.current_player += 1
-        if self.current_player == len(self.players): self.current_player = 0
+        if self.current_player == self.players.length(): self.current_player = 0
         return True
     
     def _did_player_win(self):
